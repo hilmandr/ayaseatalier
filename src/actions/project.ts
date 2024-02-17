03c15bc71d392e91edaed3dc47fd96403c29dcb0
Zulfian Fachru Reza <julianfreza04@gmail.com>
2024-02-17 21:18:48 +0700

    feat(projects): setup drizzle and add actions project

---
+++ src/actions/project.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { db } from "@/db";
+import { Project, project } from "@/db/schema";
+import { CreateProjectRequest } from "@/lib/validations/project.validation";
+
+export const createProject = async (request: CreateProjectRequest) => {
+  const newProject = await db
+    .insert(project)
+    .values({
+      title: request.title,
+      summary: request.summary,
+      content: "",
+      thumbnail: request.thumbnail,
+      client: request.client,
+      date: request.date,
+      place: request.place,
+    })
+    .returning();
+
+  return newProject;
+};
+
+export const GetProjects = async (): Promise<Project[]> => {
+  const projects = await db.query.project.findMany();
+
+  return projects;
+};
---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const project = pgTable("project", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  title: varchar("title").notNull(),
+  place: varchar("place").notNull(),
+  client: varchar("client").notNull(),
+  date: timestamp("date").notNull(),
+  summary: varchar("summary").notNull(),
+  thumbnail: varchar("thumbnail").notNull(),
+  content: varchar("content").notNull(),
+});
+
+export type Project = InferSelectModel<typeof project>;
+export type NewProject = InferInsertModel<typeof project>;
