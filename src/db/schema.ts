0b9d14e0473559a18d20c9e7719f9f646695698d
hilmandr <hilmanma513@gmail.com>
2024-02-20 16:09:24 +0700

    feat/inputDB

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, varchar, serial } from "drizzle-orm/pg-core";
 
 export const project = pgTable("project", {
   id: uuid("id").defaultRandom().primaryKey(),
