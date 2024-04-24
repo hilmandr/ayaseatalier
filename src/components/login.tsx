f1d18bee51776a89ff95152569d84dcb94a99ac6
hilmandr <hilmanma513@gmail.com>
2024-04-24 20:34:11 +0700

    fixed NextAuth Secret

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -8,8 +8,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useCallback } from "react";
 import { signIn } from "next-auth/react";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { toast } from "sonner";
+import withAuth from "next-auth/middleware";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
   const form = useForm<Login>({
     resolver: zodResolver(login),
     defaultValues: {
@@ -17,13 +21,23 @@ export default function Login() {
       password: "",
     },
   });
-  const onSubmit: SubmitHandler<Login> = useCallback(async (data) => {
-    signIn("credentials", {
-      username: data.username,
-      password: data.password,
-      callbackUrl: "/dashboard",
-    });
-  }, []);
+  const onSubmit: SubmitHandler<Login> = useCallback(
+    async (data) => {
+      const res = await signIn("credentials", {
+        username: data.username,
+        password: data.password,
+        callbackUrl: "/dashboard",
+        redirect: false,
+      });
+
+      if (res?.error) {
+        toast.error("error");
+      } else {
+        router.push("/dashboard");
+      }
+    },
+    [router]
+  );
 
   return (
     <>
@@ -34,7 +48,11 @@ export default function Login() {
             <p className=" text-sm">Please complete your details to login.</p>
             <div className=" flex flex-col pt-5 gap-y-3">
               <Form {...form}>
-                <form action="" onSubmit={form.handleSubmit(onSubmit)}>
+                <form
+                  action=""
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className=" space-y-4"
+                >
                   <FormField
                     control={form.control}
                     name="username"
@@ -63,7 +81,7 @@ export default function Login() {
                       </FormItem>
                     )}
                   />
-                  <div className=" flex mt-2">
+                  <div className=" flex">
                     <Button className=" w-full">Login</Button>
                   </div>
                 </form>
